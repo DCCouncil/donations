df91b7544be92eede3414158ca5240e4c2c3ad38
vzvenyach <vdavez@gmail.com>
2014-12-25 21:46:49 -0500

    added tastypie support

---
+++ app/api/resources.py
@@ -0,0 +1,19 @@
+from tastypie.resources import ModelResource
+from django.contrib.auth.models import User
+from tastypie import fields
+from app.models import Donation
+from tastypie.authorization import ReadOnlyAuthorization
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        excludes = ['username','email', 'password', 'is_active', 'is_staff', 'is_superuser','id','last_login','date_joined']
+
+class DonationResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'created_by', full=True)
+
+    class Meta:
+        queryset = Donation.objects.all()
+        allowed_methods = ['get']
+        authorization = ReadOnlyAuthorization()
